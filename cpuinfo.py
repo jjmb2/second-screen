9573f606bbdc70b1eb198a9f35393c515ffd9abc
Julian Janisch <julianjanisch23@gmail.com>
2023-11-03 17:05:27 +0200

    Add basic interactivity

--- cpuinfo.py
+++ cpuinfo.py
@@ -1,7 +1,10 @@
 import serial
+import threading
 import time
 
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+# global flag to stop all threads
+stop_threads = False
 
 # writes to arduino serial and then returns what the arduino sends back
 def write_read(x):
@@ -14,6 +17,19 @@ def get_temp():
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as temp:
         return int(temp.read()) / 1000
 
+# target for thread which sends temperature data to the arduino
+def send_temp():
+    while not stop_threads:
+        write_read(str(get_temp()))
+
+txt_temp_thread = threading.Thread(target=send_temp)
 
 while True:
-    write_read(str(get_temp()))
+    cmd = input("['t': temperature info, 'q': quit]" )
+
+    match cmd:
+        case 't':
+            txt_temp_thread.start()
+        case 'q':
+            stop_threads = True
+            break
