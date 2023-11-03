5b0d792657c2e2609155bdc77e312116d75d84fd
Julian Janisch <julianjanisch23@gmail.com>
2023-11-03 16:06:47 +0200

    Move temperature fetching into its own function

--- cpuinfo.py
+++ cpuinfo.py
@@ -3,12 +3,17 @@ import time
 
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
 
+# writes to arduino serial and then returns what the arduino sends back
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(2)
     data = arduino.readline()
     return data
 
-while True:
+def get_temp():
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as temp:
-        write_read(str(int(temp.read()) / 1000))
+        return int(temp.read()) / 1000
+
+
+while True:
+    write_read(str(get_temp()))
